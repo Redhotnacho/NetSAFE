ac1a6300422d543b3843f8b414eac3db4e4dcf13
yerkobanda77 <39206702+yerkobanda77@users.noreply.github.com>
2018-06-20 00:27:29 -0400

    Commit Martes Login y Menu

--- SolutionSAFE/SAFE.Web/Views/Login.aspx.designer.cs
+++ SolutionSAFE/SAFE.Web/Views/Login.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace SAFE.Web {
     public partial class Login {
         
         /// <summary>
-        /// Control form1.
+        /// Control form2.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.HtmlControls.HtmlForm form2;
         
         /// <summary>
         /// Control loginname.
@@ -56,14 +56,5 @@ namespace SAFE.Web {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lloginpass;
-        
-        /// <summary>
-        /// Control btnLogin.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnLogin;
     }
 }
